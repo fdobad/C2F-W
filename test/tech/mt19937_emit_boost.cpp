9d8100ac48b6ff343ea65b6632db9480a617f61d
fdobad <29801096+fdobad@users.noreply.github.com>
2025-12-02 18:12:41 -0300

    boost version

---
+++ test/tech/mt19937_emit_boost.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <boost/random.hpp>
+
+int main()
+{
+    const uint32_t seed = 123456789u;
+    boost::random::mt19937 eng(seed);
+
+    boost::random::uniform_int_distribution<int> udist(1, 1000000);
+    boost::random::normal_distribution<double> ndist(0.0, 1.0);
+
+    std::cout << "# boost::mt19937 uniform_int [1,1000000]" << std::endl;
+    for (int i = 0; i < 1000; ++i)
+    {
+        std::cout << udist(eng) << '\n';
+    }
+
+    eng.seed(seed);
+    std::cout << "# boost::mt19937 normal mean=0 stddev=1" << std::endl;
+    std::cout << std::setprecision(17);
+    for (int i = 0; i < 1000; ++i)
+    {
+        std::cout << ndist(eng) << '\n';
+    }
+
+    return 0;
+}
